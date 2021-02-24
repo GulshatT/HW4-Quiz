6521c4acd33053ba343d9baadb5fa197486c3d76
gigi <guka.tgs@gmail.com>
2021-02-24 18:11:26 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const startButton = document.getElementById("start-btn")
+const questionElement = document.getElementById("question")
+
+startButton.addEventListener("click", startGame)
+
+setNextQuestion()
+
+function startGame () {
+    console.log("Started")
+}
+function setNextQuestiontGame () {
+}
+
+function selectAnswer () {
+}
